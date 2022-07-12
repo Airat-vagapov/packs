23988be9c3e4eabf6ebcfb0b162e02c542aa78fe
Airat Vagapov <airlake@yandex.ru>
2022-07-12 20:20:44 +0300

    Update

--- scripts/main2.js
+++ scripts/main2.js
@@ -1,39 +1,21 @@
 // Переменные
-let citySubmitPopup = document.querySelector('.city-submit-window');
-let activeClassName = "active";
-
+const activeClassName = "active";
 
 // Добавление класса active (display: block)
-function activate (element) {
-	element.classList.add(activeClassName);
+function activate(elem) {
+  elem.classList.add(activeClassName);
 }
 
-function deActivate (element) {
-	element.classList.remove(activeClassName);
+function disable(elem) {
+	elem.classList.remove(activeClassName);
 }
 
 // Открытие подтверждения города
-window.addEventListener("load", activate(citySubmitPopup));
+let citySubmitPopup = document.querySelector(".city-submit-window");
+document.addEventListener("DOMContentLoaded", activate(citySubmitPopup));
 
 // Закрытие окна подтверждения города
-let closeBtn = document.querySelector('submit-close');
-console.log(closeBtn);
-closeBtn.addEventListener('click', deActivate(citySubmitPopup));
+let closeBtn = citySubmitPopup.querySelector(".submit-close");
 
-
-// Отображение баннера через Owl Carousel
-
-let owl = $('.owl-carousel'); 
-	owl.owlCarousel({
-		loop: true,
-		items: 1,
-	});
-	let fwdBanner = $('.banner-left').find('.arrow-fwd')
-	let prevBanner = $('.banner-left').find('.arrow-prev')
-	
-	fwdBanner.click(function () {
-		owl.trigger('next.owl.carousel', (700));
-	});
-	prevBanner.click(function () {
-		owl.trigger('prev.owl.carousel', (700));
-	});
+closeBtn.addEventListener("click", disable(closeBtn.parentNode.parentElement));
+console.log(closeBtn.parentNode.parentElement)
