9c2efab504eaf725d0e8607f3e5ea9b867218f77
Airat Vagapov <airlake@yandex.ru>
2022-08-01 23:04:21 +0300

    Head menu rewrite to JS

--- scripts/main2.js
+++ scripts/main2.js
@@ -1,22 +1,45 @@
 // Переменные
-const activeClassName = "active";
+const activeClassName = 'active',
+    subMenuActiveClass = 'submenu-active';
 
 // Добавление класса active (display: block)
 function activate(elem) {
-  elem.classList.add(activeClassName);
+    elem.classList.add(activeClassName);
 }
 
 function disable(elem) {
-	elem.classList.remove(activeClassName);
+    elem.classList.remove(activeClassName);
 }
 
 // Открытие подтверждения города
-let citySubmitPopup = document.querySelector(".city-submit-window");
-document.addEventListener("DOMContentLoaded", activate(citySubmitPopup));
+let citySubmitPopup = document.querySelector('.city-submit-window');
+document.addEventListener('DOMContentLoaded', activate(citySubmitPopup));
 
 // Закрытие окна подтверждения города
-let closeBtn = document.querySelector(".submit-close");
-closeBtn.addEventListener("click", () => {
-  disable(citySubmitPopup);
+let closeBtn = citySubmitPopup.querySelector('.submit-close');
+closeBtn.addEventListener('click', () => {
+    disable(citySubmitPopup);
 });
 
+// Выбор раздела в верхней шапке
+const headLeftMenu = document.querySelector('.menu_left'),
+    menuPoint = headLeftMenu.querySelectorAll('.menu_drop'),
+    headerTop = document.querySelector('.header_top-2');
+
+// Всплывающие подменю в шапке сайта
+openHideMenu(menuPoint);
+function openHideMenu(menuPoint) {
+    menuPoint.forEach((item) => {
+        const dropMenu = item.querySelector('.menu_drop_container');
+
+        item.addEventListener('mouseenter', (e) => {
+            if (item.classList.contains('drop-active')) {
+                dropMenu.classList.add(activeClassName);
+            }
+        });
+
+        item.addEventListener('mouseleave', () => {
+            dropMenu.classList.remove(activeClassName);
+        });
+    });
+}
