c2b7e6fb6e1ab1a8949e6c92f2e85c77e6a309e4
Airat Vagapov <airlake@yandex.ru>
2022-08-02 22:46:32 +0300

    creat callback popup JS

--- scripts/main2.js
+++ scripts/main2.js
@@ -14,61 +14,46 @@ function disable(elem) {
 function removeClass(elem, className) {
     elem.classList.remove(className);
 }
-// Активация элемента с анимацией Fade In
-function fadeIn(elem) {
-    elem.classList.add('fadeIn');
-    setTimeout(activate, 100, elem);
-    let className = 'fadeIn';
-    setTimeout(removeClass, 400, elem, className);
-}
-// Деактивация элемента с анимацией Fade Out
-function fadeOut(elem) {
-    let className = 'fadeOut';
-    elem.classList.add(className);
-    setTimeout(disable, 300, elem);
-    setTimeout(removeClass, 300, elem, className);
-}
-
 // Открытие подтверждения города
 let citySubmitPopup = document.querySelector('.city-submit-window');
 document.addEventListener('DOMContentLoaded', () => {
-    fadeIn(citySubmitPopup);
+    activate(citySubmitPopup);
 });
 
 // Закрытие окна подтверждения города
 let closeBtn = citySubmitPopup.querySelector('.submit-close');
 closeBtn.addEventListener('click', () => {
-    fadeOut(citySubmitPopup);
+    disable(citySubmitPopup);
 });
 
 // Выбор раздела в верхней шапке
 const headLeftMenu = document.querySelector('.menu_left'),
-    menuPoint = headLeftMenu.querySelectorAll('.menu_drop'),
-    headerTop = document.querySelector('.header_top-2');
+    menuPoint = headLeftMenu.querySelectorAll('.menu_drop');
 
 // Всплывающие подменю в шапке сайта
 openHideMenu(menuPoint);
 function openHideMenu(elem) {
     elem.forEach((item) => {
         const dropMenu = item.querySelector('.menu_drop_container');
-
         item.addEventListener('mouseenter', (e) => {
             if (item.classList.contains('drop-active')) {
-                fadeIn(dropMenu);
+                activate(dropMenu);
             }
         });
 
         item.addEventListener('mouseleave', () => {
-            fadeOut(dropMenu);
+            disable(dropMenu);
         });
     });
 }
 
-// Открытие попап окна
+// Открытие попап окон
 const callbackPopup = document.querySelector('.callback-popup');
 const popupBtns = document.querySelectorAll('.button_callback');
 
 openPopup(callbackPopup, popupBtns);
+closePopup(callbackPopup);
+
 function openPopup(popup, btns) {
     // Находим кнопки открытия попапа.
     // Вешаем событие на каждую кнопку
@@ -78,7 +63,50 @@ function openPopup(popup, btns) {
             if (popup.classList.contains('popup-fade') != true) {
                 return;
             }
-            fadeIn(popup);
+            activate(popup);
         });
     });
 }
+// Функция закрытия попапа по кнопке
+function closePopup(popup) {
+    const btn = popup.querySelector('.popup-close');
+
+    // Закрытие по кнопке «закрыть»
+    btn.addEventListener('click', () => {
+        disable(popup);
+    });
+
+    // Закрытие по клике вне попапа
+    popup.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target === popup || target === popup.querySelector('.popup-body')) {
+            disable(popup);
+        }
+    });
+
+    // Закрытие по кнопке Esc
+    document.addEventListener('keydown', (e) => {
+        if (e.key == 'Escape') {
+            disable(popup);
+        }
+    });
+}
+
+// Настройка чекбокса
+const checkbox = document.querySelectorAll('.checkbox');
+
+checkbox.forEach((item) => {
+    item.addEventListener('click', () => {
+        const input = item.querySelector('input');
+
+        if (input.checked != true) {
+            input.checked = true;
+        } else {
+            input.checked = false;
+        }
+    });
+});
+
+// Настройка радио кнопки
+
+
