3989309d66994299585c4343238c48a55903e134
Airat Vagapov <airlake@yandex.ru>
2022-09-18 13:54:28 +0300

    add IP defenition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,7 @@
-
 // Классы активации
 const activeClassName = 'active',
-      subMenuActiveClass = 'submenu-active',
-      disableClass = 'disable';
+    subMenuActiveClass = 'submenu-active',
+    disableClass = 'disable';
 
 // Добавление класса active (display: flex)
 function activate(elem) {
@@ -75,41 +74,55 @@ checkbox.forEach((item) => {
 });
 
 // Получить индекс элемента по клику
-function getElIndexByClick (el, clickElement) {
+function getElIndexByClick(el, clickElement) {
     let index = 0;
-    while (el = el.previousElementSibling) {
-        if(el != clickElement) {
+    while ((el = el.previousElementSibling)) {
+        if (el != clickElement) {
             index++;
         }
     }
     return index;
-};
+}
 
 // Получить индекс элемента
-function getElIndex (el) {
+function getElIndex(el) {
     let index = 0;
-    while (el = el.previousElementSibling) {
-        if(el != el.previousElementSibling) {
+    while ((el = el.previousElementSibling)) {
+        if (el != el.previousElementSibling) {
             index++;
         }
     }
     return index;
-};
+}
 
 // Получить все элементы в блоке
-function getAllElems (container) {
+function getAllElems(container) {
     let all = container.querySelectorAll('.city__select__col');
     return all;
-};
+}
 
 // Удалить все элементы в блоке
-function removeElems (container) {
+function removeElems(container) {
     let all = getAllElems(container);
     all.forEach((item) => {
         item.remove();
-    })
+    });
 }
 
+// Получить IP адрес юзера
+const getUserIp = async () => {
+    return new Promise((resolve, reject) => {
+        fetch('https://api.ipify.org?format=json')
+            .then((response) => response.json())
+            .then((data) => {
+                resolve(data.ip);
+            })
+            .catch(console.log('error'));
+    });
+};
 
-
-
+// Получаем город по IP
+const getCityByIp = (ip) => {
+    ip.then((ip) => console.log(ip));
+};
+getCityByIp(getUserIp());
