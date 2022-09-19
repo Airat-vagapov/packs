955c4fc7d318129cb3f6b4e6f306f0b05ed75242
Airat Vagapov <airlake@yandex.ru>
2022-09-19 22:14:20 +0300

    start dev submenu and return submenu logic in head

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -31,7 +31,25 @@ closeBtn.addEventListener('click', () => {
 });
 
 // Выбор раздела в верхней шапке
+const headLeftMenu = document.querySelector('.menu_left'),
+    menuPoint = headLeftMenu.querySelectorAll('.menu_drop');
 
+// Всплывающие подменю в шапке сайта
+openHideMenu(menuPoint);
+function openHideMenu(elem) {
+    elem.forEach((item) => {
+        const dropMenu = item.querySelector('.menu_drop_container');
+        item.addEventListener('mouseenter', (e) => {
+            if (item.classList.contains('drop-active')) {
+                activate(dropMenu);
+            }
+        });
+
+        item.addEventListener('mouseleave', () => {
+            disable(dropMenu);
+        });
+    });
+}
 
 // Настройка чекбокса
 const checkbox = document.querySelectorAll('.checkbox');
