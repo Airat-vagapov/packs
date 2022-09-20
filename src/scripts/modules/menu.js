c4207c85aa2b901187d08903e85c44bb4705ed45
Airat Vagapov <airlake@yandex.ru>
2022-09-20 23:31:38 +0300

    submenu position fixed

--- src/scripts/modules/menu.js
+++ src/scripts/modules/menu.js
@@ -1,5 +1,3 @@
-
-
 // Всплывающие подменю в шапке сайта
 const headLeftMenu = document.querySelector('.menu_left'),
     menuPoint = headLeftMenu.querySelectorAll('.menu_drop');
@@ -27,17 +25,21 @@ const menuEl = catalogMenu.querySelectorAll('.main-menu__item');
 // Вывод подменю
 menuEl.forEach((menu) => {
     const submenu = menu.querySelector('.submenu');
-    if(submenu) {
+    if (submenu) {
+        // Активация при наведении на меню
         menu.addEventListener('mouseenter', () => {
+            // Выравниваем подменю по позиции
+            let menuWidth = menu.offsetWidth;
+            submenu.style.left = menuWidth - 1 + 'px';
             activate(submenu);
-        })
+        });
+        // Деактивация при наведении на меню
         menu.addEventListener('mouseleave', () => {
             disable(submenu);
-        })
+        });
     } else {
+        // Убираем иконку, если нет подменю
         const icon = menu.querySelector('.icon-drop');
         icon.remove();
     }
-})
-
-
+});
