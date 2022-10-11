678400f6dd83568111d15be1aa2606a40f0dafbe
Airat Vagapov <airlake@yandex.ru>
2022-10-11 23:47:15 +0300

    upd icons add fav click logic

--- src/scripts/modules/card.js
+++ src/scripts/modules/card.js
@@ -2,8 +2,8 @@ let cards = document.querySelectorAll('.item-card');
 let cardsSmall = document.querySelectorAll('.item-card-small');
 let cardsBig = document.querySelectorAll('.item-card-big');
 
+// Собираем карточки в один массив
 let allCards = [];
-
 allCards.push.apply(allCards, cards);
 allCards.push.apply(allCards, cardsSmall);
 allCards.push.apply(allCards, cardsBig);
@@ -12,16 +12,28 @@ allCards.push.apply(allCards, cardsBig);
 let cardCount = document.querySelectorAll('.item-card-pcs');
 
 cardCount.forEach((cnt) => {
-    let cntPlus = cnt.querySelector('.qnt-plus-btn-container')
+    let cntPlus = cnt.querySelector('.qnt-plus-btn-container');
     let cntMinus = cnt.querySelector('.qnt-minus-btn');
-    let cntSum = cnt.querySelector('.qnt-choose')
+    let cntSum = cnt.querySelector('.qnt-choose');
 
     cntPlus.addEventListener('click', () => {
-        cntSum.value++
-    })
+        cntSum.value++;
+    });
     cntMinus.addEventListener('click', () => {
-        if(cntSum.value > 0) {
-            cntSum.value--
+        if (cntSum.value > 0) {
+            cntSum.value--;
         }
-    })
-})
+    });
+});
+
+// Добавление в избранное
+allCards.forEach((card) => {
+    let fav = card.querySelector('.favorite-btn');
+    if (fav != null) {
+        fav.addEventListener('click', () => {
+            const favIcon = fav.querySelector('span');
+            favIcon.classList.toggle('icon-heart');
+            favIcon.classList.toggle('icon-heart-active');
+        });
+    }
+});
